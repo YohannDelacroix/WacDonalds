4bf7ca6d3cc08369dd4ea2beff331f30e828efd5
Yohann <yohann.delacroix@hotmail.fr>
2023-01-12 14:25:00 +0100

    Payment form masks

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -7,84 +7,17 @@ import { Component } from '@angular/core';
 })
 export class PaymentComponent {
   credit!: string;
-  spaces: number = 0;
-  previousLength: number = 0;
-  expDate: string = "__/__";
+  cvc!: string;
+  expDate!: string;
+  holderName!: string;
 
+  months: number[] = Array.from(Array(12+1).keys());
+  years: number[] = []; 
 
   ngOnInit(){
-
-  }
-
-  onChangeCreditCard(): void{
-
-    /*let lastChar = this.credit.slice(-1);
-    console.log("Last char : ", lastChar)
-    if(isNaN(parseInt(lastChar,10))){
-      this.credit = this.credit.slice(0,-1);
-      return;
-    }*/
-
-    if(this.previousLength < this.credit.length){  //Adding
-      //console.log("length : " , this.credit.length-this.spaces);
-
-      
-
-      if((this.credit.length-this.spaces)%4 == 0){
-        if(this.credit.length-this.spaces < 16){
-          this.credit = this.credit + ' ';
-          this.spaces++;
-        }
-      } 
-    }
-    else{
-      //If the last character removed is a space, remove the last
-      let realLength = this.credit.length - this.spaces +1;
-      if(realLength%4 == 0 && realLength < 16){
-          this.credit = this.credit.slice(0,-1);
-          this.spaces--;
-      }
-    }
-    this.previousLength = this.credit.length;
-  }
-
-  changeCreditCard(): void{
-    console.log("ok")
-    /*let lastChar = this.credit.slice(-1);
-    console.log("Last char : ", lastChar)
-    if(isNaN(parseInt(lastChar,10))){
-      this.credit = this.credit.slice(0,-1);
-      return;
-    }*/
-
-    if(this.previousLength < this.credit.length){  //Adding
-      //console.log("length : " , this.credit.length-this.spaces);
-
-      
-
-      if((this.credit.length-this.spaces)%4 == 0){
-        if(this.credit.length-this.spaces < 16){
-          this.credit = this.credit + ' ';
-          this.spaces++;
-        }
-      } 
-    }
-    else{
-      //If the last character removed is a space, remove the last
-      let realLength = this.credit.length - this.spaces +1;
-      if(realLength%4 == 0 && realLength < 16){
-          this.credit = this.credit.slice(0,-1);
-          this.spaces--;
-      }
-    }
-    this.previousLength = this.credit.length;
-  }
-
-  controlCreditInput(e: any){
-    console.log("Keyup : ", e.key);
-    if(isNaN(parseInt(e.key, 10))){
-      
-      this.credit = this.credit.slice(0,-1);
+    console.log(new Date().getFullYear())
+    for(let i = new Date().getFullYear() - 1; i < new Date().getFullYear() + 20; i++){
+      this.years.push(i);
     }
   }
 }
